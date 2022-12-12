66768488449b4ac087e5224204adf4eb82c1a124
GaoJunHao <2529915590@qq.com>
2022-12-12 18:32:52 +0800

    CloudDemo first

--- springcloud-consumer-dept-80/src/main/java/com/siven/DeptConsumer_80.java
+++ springcloud-consumer-dept-80/src/main/java/com/siven/DeptConsumer_80.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import java.sql.SQLOutput;
+
 @SpringBootApplication
 @EnableEurekaClient //开启eureka注解
 public class DeptConsumer_80 {
--- springcloud-eureka-7001/src/main/java/com/siven/EurekaServer_7001.java
+++ springcloud-eureka-7001/src/main/java/com/siven/EurekaServer_7001.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer //表示服务端的启动类，可以接受客户端注册进来
 public class EurekaServer_7001 {
     public static void main(String[] args) {
-        System.out.println(111);
         SpringApplication.run(EurekaServer_7001.class,args);
     }
 }
